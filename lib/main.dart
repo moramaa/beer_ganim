e1e75e80216b7aaea675ad488a0015696cb4b3f8
Amira <imoramira>
2019-03-27 20:18:47 +0200

    ff

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'Contact.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
